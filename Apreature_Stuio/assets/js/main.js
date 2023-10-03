22712895f33f9ce8c353a2b384b7b0bce7f1dfc2
ankursrv <122548800+ankursrv@users.noreply.github.com>
2023-10-03 15:07:43 +0530

    Add files via upload

---
+++ Apreature_Stuio/assets/js/main.js
@@ -0,0 +1,30 @@
+// Tesit Slider 
+var swiper = new Swiper("#testi-Slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
+
